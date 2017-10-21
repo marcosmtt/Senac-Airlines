c88bde226bd53be439c3a9ef00690170684052a8
Patrick Porto <np_patrick@yahoo.com>
2017-10-21 11:58:49 -0200

    Update TemplateDosPaineis.java

--- src/main/java/local/model/TemplateDosPaineis.java
+++ src/main/java/local/model/TemplateDosPaineis.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package local.model;
 
 import local.util.LabelButtonAnimationMouseListener;
@@ -10,10 +5,6 @@ import java.awt.Component;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-/**
- *
- * @author Yuri
- */
 public class TemplateDosPaineis {
 
     JPanel painel;
