16f37ff8627c606117c0ee8c7f4e4be5548a3948
Patrick Porto <np_patrick@yahoo.com>
2017-10-23 23:15:33 -0200

    att color hover

--- src/main/java/local/util/LabelButtonAnimationMouseListener.java
+++ src/main/java/local/util/LabelButtonAnimationMouseListener.java
@@ -9,8 +9,8 @@ public class LabelButtonAnimationMouseListener implements java.awt.event.MouseLi
     private final JLabel jLabel;
     private final Color COLOR_BACKGROUND_DEFAULT;
     private final Color COLOR_FONT_DEFAULT;
-    private final Color COLOR_BACKGROUND_NEW = new Color(247, 139, 31);
-    private final Color COLOR_FONT_NEW = new Color(3, 54, 99);
+    private final Color COLOR_BACKGROUND_NEW = new Color(55, 130, 191);
+    private final Color COLOR_FONT_NEW = new Color(255, 255, 255);
     private boolean isMouseOver;
 
     public LabelButtonAnimationMouseListener(JLabel label) {
