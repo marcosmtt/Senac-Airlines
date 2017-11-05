8c5ea376f2b722313ca4925f5b60187a44d6c04d
Yuri Bento <yurinbento@hotmail.com>
2017-11-05 01:31:41 -0200

    clientesDAO fix

--- src/main/java/local/model/database/Clientes.java
+++ src/main/java/local/model/database/Clientes.java
@@ -1,27 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package local.model.database;
 
 import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
 
+/**
+ *
+ * @author Yuri
+ */
 @Entity
 @Table(name = "clientes")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Clientes.findAll", query = "SELECT c FROM Clientes c")
+    , @NamedQuery(name = "Clientes.findById", query = "SELECT c FROM Clientes c WHERE c.id = :id")
+    , @NamedQuery(name = "Clientes.findByNome", query = "SELECT c FROM Clientes c WHERE c.nome = :nome")
+    , @NamedQuery(name = "Clientes.findByRg", query = "SELECT c FROM Clientes c WHERE c.rg = :rg")})
 public class Clientes implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "nome")
+    private String nome;
+    @Basic(optional = false)
+    @Column(name = "rg")
+    private int rg;
 
-    public Long getId() {
+    public Clientes() {
+    }
+
+    public Clientes(Integer id) {
+        this.id = id;
+    }
+
+    public Clientes(Integer id, String nome, int rg) {
+        this.id = id;
+        this.nome = nome;
+        this.rg = rg;
+    }
+
+    public Integer getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getRg() {
+        return rg;
+    }
+
+    public void setRg(int rg) {
+        this.rg = rg;
     }
 
     @Override
@@ -46,7 +103,7 @@ public class Clientes implements Serializable {
 
     @Override
     public String toString() {
-        return "Clientes.java[ id=" + id + " ]";
+        return "local.model.database.Clientes[ id=" + id + " ]";
     }
-
+    
 }
